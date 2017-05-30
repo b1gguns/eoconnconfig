896a460f82a2a97102ff245edc86d2c38cca7a38
John G <gunning@gmail.com>
2017-05-30 18:01:35 +1000

    Add chrome to gitignore

--- src/App.js
+++ src/App.js
@@ -3,6 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+    handleNameChange = (event) => {
+       console.log('Not about to crash..');
+       this.setState({name: event.target.value});
+       console.log('State changed to ' + this.state.name);
+    };
+
+  constructor(props){
+    super(props);
+
+    this.state = {name: ''};
+
+    // instead using fat arrow functions to bind this
+    //this.handleNameChange = this.handleNameChange.bind(this);    
+    //this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  // handleNameChange(event) {
+  //   console.log('About to crash..');
+  //   this.setState({value: event.target.value});
+  // }
+
   render() {
     return (
       <div className="App">
@@ -11,8 +33,15 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Here we are. This is a progressive web app.
         </p>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={this.state.value} onChange={this.handleNameChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
